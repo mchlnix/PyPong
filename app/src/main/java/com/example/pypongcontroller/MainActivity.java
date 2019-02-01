cba83dc611b4f8b867f612d2ffbed68ece141c7b
Ronny <ronny.muskel@pornhub.com>
2019-02-01 20:52:29 +0100

    Button bitches

--- app/src/main/java/com/example/pypongcontroller/MainActivity.java
+++ app/src/main/java/com/example/pypongcontroller/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.pypongcontroller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButton(View button) {
+        if (button.getId()==R.id.button2)
+            Toast.makeText(this, "UP", Toast.LENGTH_SHORT).show();
+        if (button.getId()==R.id.button)
+            Toast.makeText(this, "DOWN", Toast.LENGTH_SHORT).show();
     }
 }
